9ea2204eb950f492c6ce0f21b55126502cc4a91e
Kyle Krull <kdkrull@gmail.com>
2023-02-20 12:00:23 -0600

    Remove unnecessary content

--- greeter/lib/src/test/java/com/github/kkrull/sandbox/LibraryTest.java
+++ greeter/lib/src/test/java/com/github/kkrull/sandbox/LibraryTest.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTest {
-    @Test
-    void testSomeLibraryMethod() {
-        Library classUnderTest = new Library();
-        assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
-    }
+  @Test
+  void testSomeLibraryMethod() {
+    Library classUnderTest = new Library();
+    assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
+  }
 }
