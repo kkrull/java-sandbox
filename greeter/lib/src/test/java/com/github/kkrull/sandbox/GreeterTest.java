861a73b2b69560499717814babbc53c464209ea1
Kyle Krull <kdkrull@gmail.com>
2023-02-20 12:21:29 -0600

    Make tests nice

--- greeter/lib/src/test/java/com/github/kkrull/sandbox/GreeterTest.java
+++ greeter/lib/src/test/java/com/github/kkrull/sandbox/GreeterTest.java
@@ -3,10 +3,19 @@ package com.github.kkrull.sandbox;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+
+@DisplayName("Greeter")
 class GreeterTest {
-  @Test
-  void makeGreetingAddressesTheWholeWorld() {
-    Greeter subject = new Greeter();
-    assertEquals("Hello World!", subject.makeGreeting());
+  @DisplayName("#makeGreeting")
+  @Nested
+  class makeGreeting {
+    @DisplayName("addresses the whole world")
+    @Test
+    void addressesTheWorld() {
+      Greeter subject = new Greeter();
+      assertEquals("Hello World!", subject.makeGreeting());
+    }
   }
 }
