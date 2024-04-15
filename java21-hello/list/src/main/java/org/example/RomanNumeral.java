c3f7b718f706e7aff77c45655661cad97ca2ebb5
Kyle Krull <kdkrull@gmail.com>
2024-04-15 08:11:44 -0500

    Refactor to use the loop up to the given number

--- java21-hello/list/src/main/java/org/example/RomanNumeral.java
+++ java21-hello/list/src/main/java/org/example/RomanNumeral.java
@@ -2,26 +2,8 @@ package org.example;
 
 public class RomanNumeral {
   public static String convert(int number) {
-    if(number == 1) {
-      String romanNumeral = "";
-      for (int i = 0; i < 1; i = i + 1) {
-        romanNumeral = romanNumeral + "I";
-      }
-
-      return romanNumeral;
-    }
-
-    if (number == 2) {
-      String romanNumeral = "";
-      for(int i = 0; i < 2; i = i + 1) {
-        romanNumeral = romanNumeral + "I";
-      }
-
-      return romanNumeral;
-    }
-
     String romanNumeral = "";
-    for(int i = 0; i < 3; i = i + 1) {
+    for (int i = 0; i < number; i = i + 1) {
       romanNumeral = romanNumeral + "I";
     }
 
